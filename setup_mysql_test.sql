eebdc2832a869acbbc20a1a6c31a7ec1af4f52f5
Dragonixj <delafiado@gmail.com>
2023-11-19 20:49:05 +0000

    Task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,5 @@
--- this script prepares a MySQL server for the project
--- create project testing database with the name : hbnb_test_db
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create a new user named : hbnb_test with all privileges on the db hbnb_test_db
--- with password : hbnb_test_pwd if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- granting the SELECT privlege for the user hbnb_test on the db
-GRANT SELECT ON performance_schema. * TO 'hbnb_test'@'localhost';
-FLUSH PRIVILIGES;
--- granting all privileges to the new user on hbnb_test_db
-GRANT ALL PRIVILIGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost';
-FLUSH PRIVILIGES;
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
